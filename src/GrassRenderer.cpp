b2a7888e047980dad23f6932216b03e8f8ccbd9a
whitewill1 <sceadugengan@gmail.com>
2017-06-08 12:55:23 +1200

    grassRenderer now generates a float[] of random coordinates which will be used to distribute the grass

--- src/GrassRenderer.cpp
+++ src/GrassRenderer.cpp
@@ -1,5 +1,6 @@
  #include "GrassRenderer.hpp"
-
+#include <cmath>
+#include <random>
 
 GrassRenderer::GrassRenderer() : m_shader(Shader("grassShader"))
 {
@@ -9,12 +10,24 @@ GrassRenderer::GrassRenderer() : m_shader(Shader("grassShader"))
 
 	//TODO generate an area of grass to instance over the scene
 
+	const int density = 50; //number of grass instances over square unit of worldspace
+	
+	static GLfloat vertices[density*2];
+	
+	std::mt19937 rng; //random number generator
+	rng.seed(std::random_device()());
+	std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+	
+	for(int i = 0; i<density*2;++i){
+		vertices[i] = dist(rng);
+	}
+	
 	// Send simple triangle to GPU
-	static GLfloat vertices[] = {
-		-0.5f,-0.5f,0.0f,
-		0.5f,-0.5f,0.0f,
-		0.0f,0.5f,0.0f
-	};
+	//static GLfloat vertices[] = {
+	//	-0.5f,-0.5f,0.0f,
+	//	0.5f,-0.5f,0.0f,
+	//	0.0f,0.5f,0.0f
+	//};
 
 
 	glGenVertexArrays(1, &m_VAO);
