88f86472d7ed8ad94284cc7a9ca576f71ae99e6b
whitewill1 <sceadugengan@gmail.com>
2017-06-16 13:17:19 +1200

    grass now only renders on terrain within the correct height range (-60 to 10), therre is still an issue with it rendering on random patches of rock which i cant check for

--- res/grassShader.geo.c
+++ res/grassShader.geo.c
@@ -2,9 +2,8 @@
 layout (points) in;
 layout (triangle_strip, max_vertices = 7) out;
 
-in VS_OUT {
-    vec3 color;
-} gs_in[];
+in vec4 Position[1];
+flat in int isVisible[1];
 
 const vec4 normal = vec4(0.0,0.0,-1.0,0.0);//going to have to pass this in once instance rotation is implemented
 
@@ -15,9 +14,6 @@ uniform vec3 camPos;
 
 out vec3 normal_out;
 
-//TODO take a rotation
-
-//TODO normals
 
 void grass_blade(vec4 position){
     vec4 position2 = vec4(position.x,position.y,position.z, 1.0);//take the actual position, without the rotation angle in the 4th parameter
@@ -46,11 +42,13 @@ void grass_blade(vec4 position){
     EmitVertex();
     EndPrimitive();
     vec4 rotatedNormal = rotationMatrix*normal;
-    normal_out = vec3(rotatedNormal.x,rotatedNormal.y,rotatedNormal.z); //TODO modify this based on the rotation once it is implemented
+    normal_out = vec3(rotatedNormal.x,rotatedNormal.y,rotatedNormal.z); 
 }
 
-void main() {    
-    grass_blade(gl_in[0].gl_Position);
+void main() {   
+    if(isVisible[0] == 1){
+    grass_blade(Position[0]);
+    }
 }
 
 
--- res/grassShader.vert.c
+++ res/grassShader.vert.c
@@ -3,6 +3,8 @@
 layout (location = 0) in vec4 position;
 layout (location = 1) in vec3 offset;
 
+out vec4 Position;
+flat out int isVisible;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -10,5 +12,10 @@ uniform mat4 projection;
 uniform vec3 camPos;
 
 void main() {
-	gl_Position = position+vec4(offset,0.0);
+	Position = position+vec4(offset,0.0);
+	isVisible = 1;
+
+        if(offset.y < -59 || offset.y > 10){
+          isVisible = 0;
+        }
 }
