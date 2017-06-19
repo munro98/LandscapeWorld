d97b3011372292b1ab8e0f74a77bdd4af17efefc
Florian Treier <florian.treier@gmx.net>
2017-06-19 20:36:33 +1200

    Minor changes

--- res/waterShaderNoColor.frag.c
+++ res/waterShaderNoColor.frag.c
@@ -10,8 +10,4 @@ void main()
 {
 	vec3 normal = normalize(texture(WaterNormalMap, TexCoord.st).rgb);
 	vFragColor = vec4(normal.r + 0.1, normal.g - 0.9, normal.b + 0.1, 1);
-	// vFragColor.r += 0.1;
-	// vFragColor.g -= 0.9;
-	// vFragColor.b += 0.1;
-	// vFragColor.a = 1;
 }
